02b3eacc5c4cea36a885c071ae747f7d12d03626
desicodeer <144021536+desicodeer@users.noreply.github.com>
2024-03-04 17:50:26 +0530

    add new gsap library and eventlistener on moving cursor

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let cur = document.querySelector("#cursor");
+let curBlur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",(mov)=>{
+    cur.style.left= mov.x+"px";
+    cur.style.top = mov.y+"px";
+    curBlur.style.left = mov.x-150 + "px";
+    curBlur.style.top = mov.y-150 + "px";
+})
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "100px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
+  gsap.to("#main", {
+    backgroundColor : "#000",
+    scrollTrigger:{
+        trigger: "#main",
+        scroller: "body",
+        markers:true,
+        start: "top: -30%",
+        end : "top: -90%",
+        scrub:2
+    }
+
+  })
