99864cea7c01ea72892cbcb13b300588c6a3cff2
desicodeer <144021536+desicodeer@users.noreply.github.com>
2024-03-07 17:23:43 +0530

    changes

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ document.addEventListener("mousemove",(mov)=>{
     cur.style.top = mov.y+"px";
     curBlur.style.left = mov.x-150 + "px";
     curBlur.style.top = mov.y-150 + "px";
-})
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
 
 gsap.to("#nav", {
     backgroundColor: "#000",
@@ -55,5 +69,32 @@ gsap.to("#nav", {
       start: "top 55%",
       end: "top 45%",
       scrub: 4,
+    },
+  });
+  gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+  gsap.from(".card", {
+    scale: 0.8,
+    // opacity:0,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".card",
+      scroller: "body",
+      // markers:false,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
     },
   });
